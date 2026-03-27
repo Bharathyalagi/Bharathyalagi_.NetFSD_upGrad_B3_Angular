6423fe2a8c5a8ad70afac95f010883a72d6a702f
Bharathyalagi <bharathyalagi29738@gmail.com>
2026-03-27 18:41:44 +0530

    WEEK 6 DAY 5 HANDSon ADDED

---
+++ Week 6/Day 5/Hands on/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product { ProductId = 1, ProductName = "Laptop", Category = "Electronics", Price = 50000 },
+            new Product { ProductId = 2, ProductName = "Mobile", Category = "Electronics", Price = 20000 }
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                products.Add(obj);
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            var existing = products.FirstOrDefault(p => p.ProductId == obj.ProductId);
+
+            existing.ProductName = obj.ProductName;
+            existing.Category = obj.Category;
+            existing.Price = obj.Price;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == id);
+            products.Remove(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
