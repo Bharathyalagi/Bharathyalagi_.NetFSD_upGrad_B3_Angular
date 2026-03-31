070ee8c53d9b8eb5ff34f887dd3a641f31ce6e5d
Bharathyalagi <bharathyalagi29738@gmail.com>
2026-03-31 16:19:38 +0530

    WEEK 7 DAY 1 PUSHED

---
+++ Week 7/Day 1/Hands On -/Controllers/ContactController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public static List<ContactInfo> contacts = new List<ContactInfo>();
+
+        public IActionResult Index()
+        {
+            return View(contacts);
+        }
+
+        public IActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(x => x.ContactId == id);
+            return View(contact);
+        }
+
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return RedirectToAction("Index");
+        }
+    }
+}
