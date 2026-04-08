1b5ca19db41d88cb021aad3e1db6a2875dc4a78e
Bharathyalagi <bharathyalagi29738@gmail.com>
2026-04-08 21:55:47 +0530

    Week 8 day 3 pushed

---
+++ Week 8/Day 3/Class work/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication8._3.Models
+{
+    public class Product
+    {
+        public int Id { get; set;}
+        public string Name { get; set;}
+        public double Price { get; set;}
+        public string Category { get; set; }
+
+
+    }
+}
---
+++ Week 8/Day 3/Hands on/Q1/DataAccess/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication8._3.Models;
+
+namespace WebApplication8._3.DataAccess
+{
+    public interface IContactRepository
+    {
+        Task<IEnumerable<ContactInfo>> GetAllAsync();
+        Task<ContactInfo> GetByIdAsync(int id);
+        Task AddAsync(ContactInfo contact);
+        Task UpdateAsync(int id, ContactInfo contact);
+        Task DeleteAsync(int id);
+    }
+}
