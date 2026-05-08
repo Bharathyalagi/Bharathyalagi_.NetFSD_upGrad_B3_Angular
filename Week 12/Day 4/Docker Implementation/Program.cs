e9715ce22b13babd7bd50aa581b0aeb7693d20b8
Bharathyalagi <bharathyalagi29738@gmail.com>
2026-05-08 11:36:27 +0530

    Week 12 day 4 docker stuff pushed

---
+++ Week 12/Day 4/Docker Implementation/Program.cs
@@ -0,0 +1,34 @@
+namespace DockerDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.MapOpenApi();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
